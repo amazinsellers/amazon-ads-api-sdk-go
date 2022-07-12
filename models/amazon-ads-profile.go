5768a54e4fb15cf76b2ef6dc3e6188a6025c6ca1
Premkumar Anand <amazinsellers@gmail.com>
2022-07-12 11:19:58 +0100

    add profiles model

---
+++ models/amazon-ads-profile.go
@@ -0,0 +1,16 @@
+package amazon_ads_api_models
+
+type Profile struct {
+	ProfileId   string `json:"profileId"`
+	CountryCode string `json:"countryCode"`
+	DailyBudget string `json:"dailyBudget"`
+	TimeZone    string `json:"timezone"`
+}
+
+type ProfileAccountInfo struct {
+	MarketplaceStringId string `json:"marketplaceStringId"`
+	Id                  string `json:"id"`
+	Type                string `json:"type"`
+	Name                string `json:"name"`
+	ValidPaymentMethod  string `json:"validPaymentMethod"`
+}
